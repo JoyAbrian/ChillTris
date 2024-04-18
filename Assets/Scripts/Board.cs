88493a0a0995a2a04f10f64123bc2d4536a0bb94
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-18 22:38:42 +0800

    fix tiles not spawning

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,11 +6,14 @@ using UnityEngine.Tilemaps;
 public class Board : MonoBehaviour
 {
     public Tilemap tilemap {  get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominoes;
+    public Vector3Int spawnPosition;
 
     private void Awake()
     {
         this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
 
         for (int i = 0; i < tetrominoes.Length; i++)
         {
@@ -27,10 +30,17 @@ public class Board : MonoBehaviour
     {
         int random = Random.Range(0, this.tetrominoes.Length);
         TetrominoData data = this.tetrominoes[random];
+
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(this.activePiece);
     }
 
-    public void Set()
+    public void Set(Piece piece)
     {
-
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
     }
 }
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -1,22 +1,13 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public enum Tetromino
-{
-    I,
-    O,
-    T,
-    J,
-    L,
-    S,
-    Z,
-}
+public enum Tetromino { I, O, T, J, L, S, Z }
 
 [System.Serializable]
 public struct TetrominoData
 {
-    public Tetromino tetromino;
     public Tile tile;
+    public Tetromino tetromino;
     public Vector2Int[] cells { get; private set; }
 
     public void Initalize()
